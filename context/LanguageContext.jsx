bf388d1abb7ed0c6e443239b3815dc10bc13ddd8
Shubham Singh <ssingh.cs23@gmail.com>
2026-04-28 00:10:53 +0530

    Add files via upload

---
+++ context/LanguageContext.jsx
@@ -0,0 +1,4 @@
+"use client";
+
+// Re-exporting from lib to satisfy the requested path while maintaining backwards compatibility
+export { LanguageProvider, useLanguage, useLang } from "@/lib/LanguageContext";
